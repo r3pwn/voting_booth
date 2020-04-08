6d7bd6b74dc0a9ee4405833e6010512656ba3a95
Greg Willard (r3pwn) <Wickett06@gmail.com>
2020-04-07 21:31:02 -0400

    [api] allow port number to be configured

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 from voting_booth import app
+import json
 
 if __name__ == '__main__':
-    app.run()
+  # load proper modules based on the security setting
+  with open('appinfo.json') as json_file:
+    data = json.load(json_file)
+    for p in data['appinfo']:
+      app.run(port=p['portNum'])
